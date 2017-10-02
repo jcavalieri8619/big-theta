fa236a136a9e2fe5eb6fbf8e6971af8b912bf8ea
John P Cavalieri <jcavalieri8619@gmail.com>
2017-10-02 11:10:39 -0400

    merged

---
+++ scraper/EquationScraper/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+
+class EquationscraperItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    categories = scrapy.Field()
+    equations = scrapy.Field()
+    last_item = scrapy.Field()
+    link_dist = scrapy.Field()
+
