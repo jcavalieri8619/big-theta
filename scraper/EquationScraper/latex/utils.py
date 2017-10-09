5b73b8f91412047972151b6d31338018d84e789d
John P Cavalieri <jcavalieri8619@gmail.com>
2017-10-09 15:22:29 -0400

    latex utils to strip out style commands and identify equations from expressions

---
+++ scraper/EquationScraper/latex/utils.py
@@ -0,0 +1,27 @@
+__equality_command_list = []
+
+with open('EquationScraper/latex/equality_commands.txt', 'r') as __f:
+    for __line in __f:
+        __equality_command_list.append(__line.rstrip())
+
+__MIN_SYMBOLS = 9
+
+
+def equality_commands():
+    return __equality_command_list
+
+
+def strip_styles(latex_string):
+    latex_string = latex_string.replace('\\displaystyle', '')
+    latex_string = latex_string.replace('\\textstyle', '')
+    latex_string = latex_string.replace('\\scriptstyle', '')
+    return latex_string
+
+
+def contains_equality_command(latex_string):
+    for cmd in equality_commands():
+
+        if (latex_string.find(cmd) != -1) and len(latex_string) > __MIN_SYMBOLS:
+            return cmd
+
+    return False
