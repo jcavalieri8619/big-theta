eadc1cca8a087cf01e279a4ed8ff0b86c8d5f4bc
John P Cavalieri <jcavalieri8619@gmail.com>
2017-11-26 18:38:04 -0500

    updated css and added equation click opens wikipedia page

--- big-theta-app/src/app/equation-subject/equation-subject.component.ts
+++ big-theta-app/src/app/equation-subject/equation-subject.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MathDatabaseService} from '../services/math-database/math-database.service';
 import {LatexEquation} from '../latex-equation';
+import {WindowRefService} from '../services/window-ref/window-ref.service';
 
 @Component({
   selector: 'app-equation-subject',
@@ -12,18 +13,15 @@ export class EquationSubjectComponent implements OnInit {
   @Input() subjectID: number;
   equations: LatexEquation[] = [];
 
-  constructor(private apiConnection: MathDatabaseService) { }
+  constructor(private apiConnection: MathDatabaseService, private window_ref: WindowRefService) { }
 
   ngOnInit() {
     this.apiConnection.fetchSubjectEquations(this.subjectID).subscribe( eqns => this.equations = eqns);
     console.log( 'EquationRankComponent: fetched equations by subjectID ' + this.subjectID );
   }
 
-  searchEquation(event: LatexEquation): void {
-    // if user clicks on equation in list, then search for that equation
-    // by the latex string in event.equation
-
-    // this could either generate a new graph or..
+  openEquationWikiPage(event: LatexEquation): void {
+    this.window_ref.nativeWindow.open( event.url );
   }
 
 }
